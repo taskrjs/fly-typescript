ef817035bee95bbeda44d596ce3feef036896fa0
Iegor Azuaga <iegormundo@hotmail.com>
2016-11-17 12:29:42 +0100

    Update flyfile.js

--- flyfile.js
+++ flyfile.js
@@ -1,9 +1,13 @@
 var x = module.exports
 
+x.default = function * () {
+  yield this.watch('test/src/app.ts', 'build')
+}
+
 x.build = function *() {
-  yield this.clear("test/app.js")
+  yield this.clear('test/app.js')
   yield this
-    .source("test/src/app.ts")
+    .source('test/src/app.ts')
     .typescript({
       removeComments: true,
       preserveConstEnums: true,
