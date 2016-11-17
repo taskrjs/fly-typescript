f38f4ee08450e65239062477d69ff92e2694dc23
Iegor Azuaga <iegormundo@hotmail.com>
2016-11-17 12:31:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const typescript = require("typescript")
 const assign = require("object-assign")
 
-module.exports = function (debug) {
-  this.filter("typescript", (data, options) => {
-    let result = typescript.transpile(data.toString(), assign({ module: typescript.ModuleKind.CommonJS }, options))
+module.exports = function () {
+  return this.filter("typescript", function (data, options) {
+    var result = typescript.transpile(data.toString(), assign({ module: typescript.ModuleKind.CommonJS }, options))
 
     return assign({ ext: '.js' }, { code: result })
   })
